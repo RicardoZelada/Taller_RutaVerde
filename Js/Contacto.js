9eaa22188d3afb718345dcfe2cf5ae56eaaac20e
ricardo <ricky.zelada.a@gmail.com>
2020-12-12 00:17:06 -0300

    Avances de Sitio
    Creacion de pagina Contactos.html
    Creacion de Archicos Js de contacto
    Modificacion en los estilos Css
    Carga de Imagenes
    Carga de Plugins Wow, Swiper y Archivos Bootstrap.js

--- Js/Contacto.js
+++ Js/Contacto.js
@@ -1,18 +1,17 @@
 var swiper = new Swiper('.swiper-container', {
-    effect: 'coverflow',
-    grabCursor: true,
+    spaceBetween: 30,
     centeredSlides: true,
-    slidesPerView: 'auto',
-    autoplay: true,
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
     },
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
   });
 
@@ -32,6 +31,3 @@ var swiper = new Swiper('.swiper-container', {
     section.className = 'section--purple wow fadeInDown';
     this.parentNode.insertBefore(section, this);
   };
-
-  
-
