a7c7136c27686991f11265816b51ba2a952397fd
ricardo <ricky.zelada.a@gmail.com>
2020-12-17 03:06:48 -0300

    Finalizacion
    Creacion de las Paginas de Experiencias
    Carga de Imagnes nuevas
    Modificacion en el Archivo Css

--- Js/Experiencia.js
+++ Js/Experiencia.js
@@ -8,3 +8,35 @@ wow = new WOW(
     }
   );
   wow.init();
+
+  var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  new Splide( '.splide', {
+    perPage : 2,
+    cover   : true,
+    lazyLoad: 'nearby',
+    autoplay: 'true',
+    resetProgress: 'true',
+    height  : '20rem',
+    type: 'loop',
+    speed: 3,
+    breakpoints: {
+      height: '8rem',
+    }
+  } ).mount();
+
